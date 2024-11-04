f5bf4d189a9e06a0e03cb50bda320f7a591a0533
prashant <puranikprashant3@gmail.com>
2024-11-05 00:15:47 +0530

    minor fixes

--- src/Components/products/EditProduct.js
+++ src/Components/products/EditProduct.js
@@ -74,14 +74,7 @@ export default function EditProduct() {
       <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={description}
               onChange={(e) => setDescription(e.target.value)}/>
     </div>
-    <div className="mb-5">
-    <div className="flex items-center justify-start w-full">
-    <label className="flex flex-col items-center px-4 py-0 bg-blue-500 text-white shadow-lg tracking-wide border border-blue-500 cursor-pointer hover:bg-blue-600">
-      <span>Select a file</span>
-      <input type="file" className="hidden" />
-    </label>
-  </div>
-  </div>
+
     
     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
     <ErrorMessage error={error}/>
